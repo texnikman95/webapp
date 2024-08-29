b47dd11e81fbb7f0decb051e51c61507fe21b5e4
texnikman95 <64158359+texnikman95@users.noreply.github.com>
2024-08-29 21:16:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          salom Azimjon9515
+          salom Azimjon951salom dunyo
         </a>
       </header>
     </div>
