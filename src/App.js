61bf55f751f4fe224e325549131f588a66353d05
texnikman95 <azimjon9515@gmail.com>
2024-11-26 19:26:41 +0500

    qoshildi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://t.me/texnikmanadmin"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          salom dunyo
-        </a>
-      </header>
+      salom dunyo
+      nima gapalar
+     
     </div>
   );
 }
